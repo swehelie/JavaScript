1546ba28db0bbf4d62b948980bfddbe8a488f9e2
Sarah Wehelie <42350056+swehelie@users.noreply.github.com>
2020-08-04 19:29:20 -0700

    Add files via upload

---
+++ agetodogyears/dogyears.js
@@ -0,0 +1,13 @@
+// my age
+let myAge = 19;
+// early age
+let earlyYears = 2;
+earlyYears *= 10.5;
+// later age
+let laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+// my name in lower case
+let myName = 'Sarah'.toLowerCase();
+console.log("My name is " + myName + ". I am " + myAge + " years old in human years which is " +
+    myAgeInDogYears +" in dog years.");
